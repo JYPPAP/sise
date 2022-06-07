0e4ea92f8bd7fd7e5a375f93247368f7d77d0f75
JYPPAP <yungsu222@naver.com>
2022-06-07 17:17:16 +0900

    테스트 작성 값 출력 확인

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import data from './sise.json';
 import './index.css';
 
-class Root extends React.Component {
-  
+class HeaderTop extends React.Component {
+  render() {
+    /* 
+    * TEST
+    ? TEST
+    ! TEST
+    TODO TEST
+    @param TEST
+    red
+     */
+    console.log("");
+    console.log("data");
+    console.dir(data[0].create_date);
+    console.log(data[0].create_date);
+    console.log("🚀 ~ file: index.js ~ line 20 ~ HeaderTop ~ render ~ data[0].create_date", data[0].create_date)
+    console.log("");
+    const list_value = (data[0].game_list).map((value, i) => {
+      console.log(value);
+      console.log(i);
+      return (
+      <li key={i} className="item">
+        {value.game_name}
+        <p>rank : {value.rank}</p>
+        <p>standard : {value.standard}</p>
+      </li>
+      )
+    });
+    
+    return (
+      <div id="header_top" >
+        <div className="rank_date">{data[0].create_date}</div>
+        <ul id="list">
+        {list_value}
+        </ul>
+      </div>
+    );
+  }
 }
+
+class Root extends React.Component {
+  render() {
+    return (
+      <HeaderTop />
+    );
+  }
+}
+
 
 
 // ============================
