86ba0a6a8a984f3a27d4264318b8464c1813cd34
JYPPAP <yungsu222@naver.com>
2022-06-08 18:54:13 +0900

    테스트용 영역 생성 및 분리

--- src/index.js
+++ src/index.js
@@ -6,10 +6,30 @@ import './index.css';
 class HeaderTop extends React.Component {
   render() {
     return(
-      <div></div>
+      <div id="header_top">
+        <div className="link"></div>
+        <div className="logo"></div>
+        <div className="config_app"></div>
+      </div>
     )
   }
 }
+class HeaderTab extends React.Component {
+  render() {
+    return(
+      <>
+        <ul id="header_tab">
+          <li className="home">홈</li>
+          <li className="search_game">게임검색</li>
+          <li className="favorite_game">관심게임</li>
+          <li className="trade_rank">거래순위</li>
+        </ul>
+        <div id="tab_indicate"></div>
+      </>
+    )
+  }
+}
+
 function Detail(props) {
   console.log(props);
   return (
@@ -27,28 +47,36 @@ class Container extends React.Component {
     console.log("data");
     console.log(data[0].create_date);
     console.log("");
+    let j=1;
     const list_value = (data[0].game_list).map((value, i) => {
-      console.log("🚀 ~ file: index.js ~ line 32 ~ Container ~ constlist_value= ~ this", this)
-      console.dir(this);
-      console.log(i);
+      // console.log("🚀 ~ file: index.js ~ line 32 ~ Container ~ constlist_value= ~ this", this)
+      // console.dir(this);
+      // console.log(i);
+      console.log("j");
+      console.log(j);
+      
+      if(j > 12)return;
+      j++;
       return (
         <li key={i} className="item">
           <Detail
           value = {value.game_name}
-          onClick={() => this.props.onClick(value.game_name)}
+          // onClick={() => this.props.onClick(value.game_name)}
           />
-          <p>rank : {value.rank}</p>
-          <p>standard : {value.standard}</p>
+          {/* <p>rank : {value.rank}</p>
+          <p>standard : {value.standard}</p> */}
         </li>
       )
     });
     
     return (
       <div id="container" >
-        <div className="rank_date">{data[0].create_date}</div>
-        <ul id="list">
-        {list_value}
-        </ul>
+        <div className="cont">
+          <div className="icon_home">대표 게임</div>
+          <ul id="list">
+          {list_value}
+          </ul>
+        </div>
       </div>
     );
   }
@@ -59,6 +87,7 @@ class Root extends React.Component {
     return (
       <>
         <HeaderTop />
+        <HeaderTab />
         <Container />
       </>
     );
