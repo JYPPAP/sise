d1bfe210f65de6466f91348358b5961b15fcaf69
JYPPAP <yungsu222@naver.com>
2022-06-16 18:37:23 +0900

    단순화, 훅 사용 현재까진 지금이 가장 괜찮은 상태.

--- src/index.js
+++ src/index.js
@@ -7,58 +7,90 @@ import './index.css';
 import './loading.css';
 
 
-function HeaderTop(props) {
-  // console.log("");
-  // console.log("##############");
-  // console.log("HeaderTop");
-  // console.log("props");
-  // console.log(props);
-  // console.log("##############");
-  // console.log("");
-  return(
-    <div id="header_top">
-      <div className="link"></div>
-      <div className="logo"></div>
-      <div className="config_app"></div>
-    </div>
-  )
+const HeaderTop = ({tab, sub}) => {
+  if (!Boolean(sub)) {
+    return(
+      <div id="header_top">
+        <div className="link"></div>
+        <div className="logo"></div>
+        <div className="config_app"></div>
+      </div>
+    );
+  } else {
+    return(
+      <div id="header_top">
+        <div className="back"></div>
+        <div className="title">{sub}</div>
+        {/* <div className="star"></div> */}
+      </div>
+    )
+  }
 }
 
-const MenuTab = ({tabValue, setTabValue}) => {
-  console.log("");
-  console.log("##############");
-  console.log("MenuTab");
-  console.log("{tabValue, setTabValue}");
-  console.log({tabValue, setTabValue});
-  console.log("##############");
+const MenuTab = ({tab, setTab, sub}) => {
+  if (Boolean(sub)) return;
   return(
     <>
       <ul id="menu_tab">
-        <li className="home" onClick={() => setTabValue("home")} >홈</li>
-        <li className="search_game" onClick={() => setTabValue("search")}>게임검색</li>
-        <li className="favorite_game" onClick={() => setTabValue("favorite")}>관심게임</li>
-        <li className="trade_rank" onClick={() => setTabValue("rank")}>거래순위</li>
+        <li className="home" onClick={() => setTab("home")} >홈</li>
+        <li className="search_game" onClick={() => setTab("search")}>게임검색</li>
+        <li className="favorite_game" onClick={() => setTab("favorite")}>관심게임</li>
+        <li className="trade_rank" onClick={() => setTab("rank")}>거래순위</li>
       </ul>
-      <div id="tab_indicate" className={tabValue}></div>
+      <div id="tab_indicate" className={tab}></div>
     </>
   )
 }
 
+function Container({tab, sub, setSub}) {
+  if (!Boolean(sub)) {
+    switch(tab) {
+      case "search":
+        return <SearchList tab={tab} setSub={setSub} />
+      case "favorite":
+        return <FavoriteList tab={tab} setSub={setSub} />
+      case "rank":
+        return <RankList tab={tab} setSub={setSub} />
+      case "home":
+      default:
+        return <HomeList tab={tab} setSub={setSub} />;
+    }
+  } else {
+    switch(sub) {
+      case "game":
+        return <GameList sub={sub} setSub={setSub} />
+      case "server":
+        return <ServerList sub={sub} setSub={setSub} />
+      default: return;
+    }
+  }
+}
 
-function Container(props) {
-  // console.log("");
-  // console.log("##############");
-  // console.log("Container");
-  // console.log("props");
-  // console.log(props);
-  // console.log("game_list");
-  // console.log(game_list);
-  // console.log("##############");
-  // console.log("");
-  const home_list = game_list.slice(0, 12);
-  const list_value = (home_list).map((item, i) => {
+function IconWrap({tab}) {
+  let subTitle;
+  switch(tab) {
+    case "favorite":
+      subTitle = "관심 게임"
+      break;
+    case "rank":
+      subTitle = "2022년 06월 16일 순위"
+      break;
+    case "home":
+    default:
+      subTitle = "대표 게임"
+      break;
+  }
+  return (
+    <div className="icon_wrap">
+      <span className="icon"></span>{subTitle}
+    </div>
+  )
+}
+
+function HomeList({tab, setSub}) {
+  const list_value = game_list.slice(0, 12).map((item, i) => {
     return (
-      <li key={i} className="item">
+      <li key={i} className="item" onClick={() => setSub("game")}>
         {item.game_name}
       </li>
     );
@@ -66,16 +98,46 @@ function Container(props) {
   return (
     <div id="container" >
       <div className="cont">
-        <div className="icon_home">대표 게임</div>
+        <IconWrap tab={tab} />
         <ul id="list">
-        {list_value}
+          {list_value}
         </ul>
       </div>
     </div>
   );
 }
+function SearchList({tab, setTab}) {
+  return (
+    <div id="container" ></div>
+  );
+}
+function FavoriteList({tab, setTab}) {
+  return (
+    <div id="container" ></div>
+  );
+}
+function RankList({tab, setTab}) {
+  return (
+    <div id="container" ></div>
+  );
+}
 
-function Loading() {
+function GameList({sub, setSub}) {
+  return (
+    <div id="container" ></div>
+  );
+}
+function ServerList({sub, setSub}) {
+  return (
+    <div id="container" ></div>
+  );
+}
+
+function Loading({setIsReady}) {
+  // setIsReady
+  /* 
+  onClick={() => setIsReady(true)}
+  */
   return(
     <div class="loading_wrap">
       <div className="load-dots">
@@ -92,22 +154,23 @@ function Loading() {
 
 const App = () => {
   const [isReady, setIsReady] = useState(true);
-  const [tabValue, setTabValue] = useState("home");
+  const [tab, setTab] = useState("home");
+  const [sub, setSub] = useState("");
   if(isReady === true) {
     return(
       <>
-        <HeaderTop tabValue={tabValue} setTabValue={setTabValue} />
-        <MenuTab tabValue={tabValue} setTabValue={setTabValue} />
-        <Container tabValue={tabValue} setTabValue={setTabValue} />
+        <HeaderTop tab={tab} setTab={setTab} sub={sub} />
+        <MenuTab tab={tab} setTab={setTab} sub={sub} />
+        <Container tab={tab} sub={sub} setSub={setSub} />
       </>
     )
   } else {
     return(
-      <Loading isReady={isReady} setIsReady={setIsReady} />
+      <Loading setIsReady={setIsReady} />
     );
   }
 }
-// ============================
+
 const app = ReactDOM.createRoot(document.getElementById("root"));
 app.render(<App />);
 
